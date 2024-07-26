b420839e84ab176521c8a515a36273101820d258
muchmuch <60392231+ledlaux@users.noreply.github.com>
2024-07-27 00:02:09 +0300

    Update midiharmonium.sh

--- midiharmonium.sh
+++ midiharmonium.sh
@@ -1,7 +1,11 @@
 #! /bin/sh 
 
+# Disconnect all previous midi connections
 aconnect -x
 
+# Run fluidsynth with small delay to allow passing of the booting process
+
 fluidsynth -a alsa -z 160 -m alsa_raw -o midi.alsa.device=hw:2,0 -g 1 /home/pi/midiharmonium.sf2 
+
 sleep 10
 
