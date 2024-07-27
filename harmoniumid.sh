99ee7485402fcd276fd4f01e3ae40347ff25d72f
Vadims Maksimovs <60392231+ledlaux@users.noreply.github.com>
2024-07-27 16:26:34 +0300

    Update and rename midiharmonium.sh to harmoniumid.sh

--- harmoniumid.sh
+++ harmoniumid.sh
@@ -5,7 +5,7 @@ aconnect -x
 
 # Run fluidsynth with small delay to allow passing of the booting process
 
-fluidsynth -a alsa -z 160 -m alsa_raw -o midi.alsa.device=hw:2,0 -g 1 /home/pi/midiharmonium.sf2 
+fluidsynth -a alsa -z 160 -m alsa_raw -o midi.alsa.device=hw:2,0 -g 1 /home/pi/harmoniumid.sf2 
 
 sleep 10
 
